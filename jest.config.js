7da4f56b05a147f784842e31463a11616f9258e9
xbrokenSky <doinikov@gmx.com>
2019-01-05 21:06:47 +0300

    added enzyme, enzyme-to-json for snapshot testing and some jest settings

--- jest.config.js
+++ jest.config.js
@@ -9,4 +9,16 @@ module.exports = {
         '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/__mocks__/fileMock.js',
         '\\.(css|less|styl|scss|sass)$': 'identity-obj-proxy',
     },
+    verbose: true,
+    notify: true,
+    collectCoverage: true,
+    collectCoverageFrom: ['src/scripts/**'],
+    coverageThreshold: {
+        global: {
+            branches: 80,
+            functions: 80,
+            lines: 80,
+            statements: 80
+        }
+    }
 };
